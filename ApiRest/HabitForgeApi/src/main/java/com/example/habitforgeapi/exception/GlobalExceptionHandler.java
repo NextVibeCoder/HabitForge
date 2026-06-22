f417b426ad76859d3d1636800f03133ccc003825
NextVibeCoder <164054592+NextVibeCoder@users.noreply.github.com>
2026-06-21 20:48:17 -0600

    Gestion de errores agregada

---
+++ ApiRest/HabitForgeApi/src/main/java/com/example/habitforgeapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.habitforgeapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?>  BadRequestExceptionHandeler(BadRequestException ex){
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value(), "Solicitud incorrecta");
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?>  ResourceNotFoundExceptionHandeler(ResourceNotFoundException ex){
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), "Recurso no encontrado");
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> MethodArgumentNotValidExceptionHandeler(MethodArgumentNotValidException ex){
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        ErrorResponse error = new ErrorResponse("Error de validacion en los campos " + String.join(", ", errors.keySet()), HttpStatus.BAD_REQUEST.value(), " Validacion fallida");
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+
+}
---
+++ ApiRest/HabitForgeApi/src/main/java/com/example/habitforgeapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.habitforgeapi.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
