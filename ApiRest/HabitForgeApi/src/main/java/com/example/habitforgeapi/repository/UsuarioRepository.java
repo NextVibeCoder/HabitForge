396b6feff0144d5384b0e9ff7d04cbc6bcd88e95
NextVibeCoder <164054592+NextVibeCoder@users.noreply.github.com>
2026-06-21 22:07:10 -0600

    Usuario Repository agregado

---
+++ ApiRest/HabitForgeApi/src/main/java/com/example/habitforgeapi/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.habitforgeapi.repository;
+
+import com.example.habitforgeapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+    public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+        Optional<Usuario> findByEmail(String email);
+    }
+
