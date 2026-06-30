faedb45880c3b74ce5cd9a6d175821b9ef2223b2
J-nextwebdev <jsvilchez@uamv.edu.ni>
2026-06-29 20:29:07 -0600

    Feat(viewmodel, service, model, repository, screen): El perfil recupera datos de la api

--- AppMovil/app/src/main/java/com/example/habitforge/ui/navigation/Navigation.kt
+++ AppMovil/app/src/main/java/com/example/habitforge/ui/navigation/Navigation.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.example.habitforge.ui.screen.*
+import com.example.habitforge.ui.viewmodel.AppViewModelProvider
 
 @Composable
 fun Navigation() {
@@ -86,6 +88,7 @@ fun Navigation() {
 
             composable<Profile> {
                 ProfileScreen(
+                    viewModel = viewModel(factory = AppViewModelProvider.Factory),
                     onNavigateToHome = { navController.navigate(Home) },
                     onNavigateToLog = { navController.navigate(Log) },
                     onNavigateToSquad = { navController.navigate(Friends) },
